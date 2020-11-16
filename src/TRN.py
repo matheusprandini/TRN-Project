8d4accf4569a3851d63fab96052d964e7490ca4e
matheusprandini <matheusprandini.96@gmail.com>
2020-11-16 19:03:27 -0300

    Update TRN model

--- src/TRN.py
+++ src/TRN.py
@@ -1,39 +1,30 @@
 import keras as K
-import numpy as np
+from keras.models import Model
+from keras.layers import Input, Dense, Flatten, Reshape, Dropout
+from keras.layers.recurrent import LSTM
+from keras.layers.merge import concatenate
 from keras.applications.vgg16 import VGG16
-from keras.preprocessing.image import ImageDataGenerator
 
-'''vgg16_model = VGG16(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
+vgg16_model = K.models.Sequential()
+vgg16_model.add(VGG16(weights="imagenet", include_top=False, input_shape=(224, 224, 3)))
+vgg16_model.add(Flatten())
+vgg16_model.add(Dense(4096, activation="relu"))
+vgg16_model.add(Dense(4096, activation="relu"))
 
-cnn_model = K.models.Sequential()
-cnn_model.add(vgg16_model)
-cnn_model.add(K.layers.Flatten())
-cnn_model.add(K.layers.Dense(4096, activation="relu"))
-cnn_model.add(K.layers.Dense(4096, activation="relu"))
-cnn_model.add(K.layers.LSTM(units=10))'''
+lstm_model = K.models.Sequential()
+lstm_model.add(Reshape((4096,1)))
+lstm_model.add(Dropout(.1))
+lstm_model.add(LSTM(4096))
+lstm_model.add(Reshape((4096,1)))
+lstm_model.add(Dropout(.1))
+lstm_model.add(LSTM(4096))
+lstm_model.add(Dense(3))
 
-'''for layer in cnn_model.layers:
-    layer.trainable = False'''
+input_shape = (224, 224, 3)
+visible = Input(shape=input_shape)
+features = vgg16_model(visible)
+reshaped_features = Reshape((4096,1))(features)
+classifier = lstm_model(reshaped_features)
 
-'''lstm_model = K.models.Sequential()
-lstm_model.add(K.layers.LSTM(10, return_sequences=False, return_state=True, input_shape=(4096,1)))
-
-classifier = K.models.Sequential()
-classifier.add(K.layers.Dense(3))
-
-model = TimeDistributed(cnn_model)
-model = rnn(lstm_model)
-model = dense(classifier)'''
-
-'''model_input = K.layers.Input(shape=(1, 224, 224, 3))
-features = cnn_model(model_input)
-output, state_h, state_c = K.layers.LSTMCell(10)(features)
-classifier = K.layers.Dense(3)(output)'''
-
-'''model = K.models.Sequential()
-model.add(K.layers.LSTM(10, input_shape=(4096,1)))
-print(model.summary())'''
-
-x = K.layers.Input(shape=(4096,1))
-print(x.shape)
-output, state_h, state_c = K.layers.LSTM(units=10)(x)
+model = Model(inputs=visible, outputs=classifier)
+print(model.summary())
